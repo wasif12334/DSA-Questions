8678015e84bd458088f7f6511437f58d9d179720
wasif12334 <kashifaliraza95@gmail.com>
2024-09-25 12:45:38 +0500

    added 2 pattern question to repo

---
+++ Array/pattren question/sqaure_matrixi_in_different_order.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=1;
+    // cin>>n;
+    for(int i=1;i<=3;i++)
+    {
+        for(int j=1;j<=3;j++){
+         cout<<n<<" ";
+       n++;
+       }
+       cout<<endl;
+    }
+}
