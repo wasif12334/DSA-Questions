93b72c78840a088305cbc1e4f17f1e4182b1f98e
wasif12334 <kashifaliraza95@gmail.com>
2024-12-02 09:07:47 +0500

    added the question in which i add two sorted linked list

--- linked _list/Add two list.cpp
+++ linked _list/Add two list.cpp
@@ -29,11 +29,24 @@ void append(int val){
 }
 void show(){
 	Node* temp = head ;
+	cout<<"[";
 		while(temp!=NULL){
-		cout<<temp->data<<" -> ";
+		cout<<temp->data<<" ";
 		temp = temp->next;
 	}
-	cout<<" NULL"<<endl;
+	cout<<"]"<<endl;
+}
+void reverse(){
+	Node *p_node,*c_node,*n_node;
+	p_node = NULL;
+	c_node = n_node = head;
+	while(n_node!=NULL){
+		n_node = n_node->next;
+		c_node->next = p_node;
+		p_node = c_node;
+		c_node = n_node;
+	}
+	 head=p_node;
 }
 };
 
@@ -51,20 +64,26 @@ Linked_list Adding_LinkedList(Linked_list l1,Linked_list l2){
 	}
 	return result;
 }
+
 int main(){
 	Linked_list l1,l2,l3;
 	l1.append(1);
 	l1.append(2);
-	cout<<"\nLinked List No 1 : \n\n";
+	l1.append(3);
+	cout<<"\nLinked List No 1 : ";
+	l1.reverse();
 	l1.show();
 	
-	l2.append(2);
 	l2.append(4);
-	l2.append(44);
-	cout<<"\nLinked List No 2 : \n\n";
+	l2.append(5);
+	l2.append(6);
+l2.reverse();
+	cout<<"\nLinked List No 2 : ";
 	l2.show();
 	l3 = Adding_LinkedList(l1,l2);
-	cout<<"\nAdded Linked List  : \n\n";
+	cout<<"\nAdded Linked List  : ";
+	l3.reverse();
 	l3.show();
+
 	
 }
